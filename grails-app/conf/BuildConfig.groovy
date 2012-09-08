76e5b8111620ab16176b1da7c1fb940acf73105d
Cedric G Hurst II <cedric@spantree.net>
2012-09-08 14:44:20 -0500

    #17: moving rhino dep to build scope

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -12,10 +12,12 @@ grails.project.dependency.resolution = {
     repositories {
         grailsCentral()
         grailsRepo "http://grails.org/plugins"
+        mavenCentral()
     }
     dependencies {
       // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
       build 'com.googlecode.jarjar:jarjar:1.3'
+      build 'org.mozilla:rhino:1.7R3'
     }
     plugins {
         build ":release:2.0.0", {
